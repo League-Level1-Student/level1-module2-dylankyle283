4ab878404691eb17b5c5b9e0d8333bdeefd5d1d4
League Student <league@iMac-22.attlocal.net>
2019-08-05 16:26:13 -0700

    i started getters and setters

---
+++ src/_01_getters_and_setters/taco_runner.java
@@ -0,0 +1,10 @@
+package _01_getters_and_setters;
+
+public class taco_runner {
+public static void main(String[] args) {
+	taco yes = new taco();
+
+setSauce();
+
+}
+}
---
+++ src/_06_minion/Minion.java
@@ -0,0 +1,24 @@
+package _06_minion;
+
+public class Minion {
+	private String name; 
+		private int eyes; 
+		private String color; 
+		private String master;
+
+
+void getName(){
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+}
